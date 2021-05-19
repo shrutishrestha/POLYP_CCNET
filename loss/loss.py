304910be22b21e32f449c30e029bffdd48597a63
shrutishrestha <shruti.shresthaa@gmail.com>
2021-05-19 18:27:38 +0545

    changes in loss file

--- loss/loss.py
+++ loss/loss.py
@@ -20,25 +20,19 @@ class OhemCrossEntropy2d(nn.Module):
     def find_threshold(self, np_predict, np_target):
         # downsample 1/8
         factor = self.factor
-
-        predict = nd.zoom(np_predict, (1.0, 1.0, 1.0/factor, 1.0/factor), order=1) #(2, 2, 96, 96)
-        target = nd.zoom(np_target, (1.0, 1.0/factor, 1.0/factor), order=0) #(2, 96, 96)
+        predict = nd.zoom(np_predict, (1.0, 1.0, 1.0/factor, 1.0/factor), order=1)
+        target = nd.zoom(np_target, (1.0, 1.0/factor, 1.0/factor), order=0)
 
         n, c, h, w = predict.shape
         min_kept = self.min_kept // (factor*factor) #int(self.min_kept_ratio * n * h * w)
 
-        input_label = target.ravel().astype(np.int32) #(18432,)
+        input_label = target.ravel().astype(np.int32)
+        input_prob = np.rollaxis(predict, 1).reshape((c, -1))
 
-        input_prob = np.rollaxis(predict, 1).reshape((c, -1)) #(2, 18432)
-
-        valid_flag = input_label != self.ignore_label #(18432,)
-
-        valid_inds = np.where(valid_flag)[0] #(18432,)
-
-        label = input_label[valid_flag] #(18432,)
-
+        valid_flag = input_label != self.ignore_label
+        valid_inds = np.where(valid_flag)[0]
+        label = input_label[valid_flag]
         num_valid = valid_flag.sum()
-
         if min_kept >= num_valid:
             threshold = 1.0
         elif num_valid > 0:
@@ -51,30 +45,23 @@ class OhemCrossEntropy2d(nn.Module):
                 new_threshold = new_array[k_th]
                 if new_threshold > self.thresh:
                     threshold = new_threshold
-
         return threshold
 
 
     def generate_new_target(self, predict, target):
         np_predict = predict.data.cpu().numpy()
         np_target = target.data.cpu().numpy()
-
         n, c, h, w = np_predict.shape
 
         threshold = self.find_threshold(np_predict, np_target)
 
-        input_label = np_target.ravel().astype(np.int32) #(591361,)
+        input_label = np_target.ravel().astype(np.int32)
+        input_prob = np.rollaxis(np_predict, 1).reshape((c, -1))
 
-        input_prob = np.rollaxis(np_predict, 1).reshape((c, -1)) #(2, 591361)
-
-        valid_flag = input_label != self.ignore_label #591361,)
-
-        valid_inds = np.where(valid_flag)[0] #(591361,)
-
-        label = input_label[valid_flag] #(591361,)
-
-        num_valid = valid_flag.sum() #591361
-
+        valid_flag = input_label != self.ignore_label
+        valid_inds = np.where(valid_flag)[0]
+        label = input_label[valid_flag]
+        num_valid = valid_flag.sum()
 
         if num_valid > 0:
             prob = input_prob[:,valid_flag]
@@ -86,7 +73,8 @@ class OhemCrossEntropy2d(nn.Module):
         label = input_label[valid_inds].copy()
         input_label.fill(self.ignore_label)
         input_label[valid_inds] = label
-        new_target = torch.from_numpy(input_label.reshape(target.size())).long()
+
+        new_target = torch.from_numpy(input_label.reshape(target.size())).long().cuda(target.get_device())
 
         return new_target
 
@@ -100,6 +88,9 @@ class OhemCrossEntropy2d(nn.Module):
                                            If given, has to be a Tensor of size "nclasses"
         """
         assert not target.requires_grad
+
         input_prob = F.softmax(predict, 1)
+
         target = self.generate_new_target(input_prob, target)
+
         return self.criterion(predict, target)
